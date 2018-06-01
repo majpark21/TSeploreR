fdfa6dcffe49bb99ce3210a0b73692246bbbe2a6
majpark21 <marc-antoine.jacques@izb.unibe.ch>
2018-06-01 18:41:53 +0200

    extract peak includes recentering/filtering/extrapolation for early peaks

--- R/peak_extraction.R
+++ R/peak_extraction.R
@@ -1,6 +1,7 @@
 #' Extract Peak
 #'
-#' A wrapper for different peak extraction methods.
+#' A wrapper for different peak extraction methods. In addition, has some options
+#' to detect peak position to give a more precise estimate of peak position.
 #' @param y numeric vector. The series from which to extract peak positions.
 #' @param method Peak extraction method(s). One or several of ("zscore",
 #'   "spike", "bump", "wavelet", "extrema").
@@ -44,6 +45,20 @@
 #' @param ext.what One of "mini" or "maxi". For "extrema" method.
 #' @param show.warning Logical, disable warning for the current execution. Useful when providing
 #' all.winlen and applying the function multiple times.
+#' @param recenter numeric or NULL. If numeric, attempt to reposition peak position to a more precise
+#' location. Specifically, starting from peak estimated position, look for maximum in a window centered
+#' around it. The length of the window corresponds to the parameter value. Highly recommended for "spike"
+#' and "bump" method which tend to give an imprecise estimation of peak position.
+#' @param filter.thresh numeric or NULL. If numeric, attempt at filtering peaks on the base of a 
+#' minimal amplitude. To do so, TS are detrended by subtracting a rolling mean and
+#' amplitude to baseline is computed. Peak is kept if this amplitude is above the "filter" value.
+#' If NULL, no filter is applied. It is highly recommended to recenter the peaks if this option is used.
+#' @param filter.winlen size of the window for moving average used to detrend prior to filtering. It is
+#' highly recommended to check the detrended signal (part of the output if filter is specified): in the
+#' detrended signal, area between peaks should oscillate around 0. If not, try to adjust the value.
+#' @param detect.early numeric or NULL. If numeric, extrapolate the time series by the corresponding
+#' number of points at its start. This aims at improving peak detection at the beginning of the series.
+#' Extrapolation is performed by ARIMA model (see ?forecast::auto.arima)
 #'
 #' @details Zscore method is based on
 #'   https://stackoverflow.com/questions/22583391/peak-signal-detection-in-realtime-timeseries-data#22640362.
@@ -58,7 +73,8 @@
 #'   https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.find_peaks_cwt.html.
 #'
 #' @return A data table containing the signal along with results all of selected
-#'   methods. Each method result is stored in a different column.
+#'   methods. Each method result is stored in a different column. If filter is specified,
+#'   also returns the detrended signal.
 #' @export
 #' 
 extract_peak <-
@@ -82,10 +98,17 @@ extract_peak <-
            wt.centile_noise = 10,
            ext.winlen = NULL,
            ext.what = "maxi",
+           recenter = 2,
+           filter.thresh = NULL,
+           filter.winlen = all.winlen*2,
+           detect.early = NULL,
            show.warning = TRUE) {
     require(data.table)
     require(peakPick)
     require(reticulate)
+    
+    # Save vector with call for detect.early recursive call
+    argList <- c(as.list(environment()))
     
     vmethod = c("zscore", "spike", "bump", "wavelet", "extrema")
     if (any(!method %in% vmethod)) {
@@ -195,6 +218,68 @@ extract_peak <-
       v.ext <- rep(FALSE, length(y))
       v.ext[pos.peaks] <- TRUE
       dt.out[, extrema := v.ext]
+    }
+    
+    # Extrapolation for early peaks
+    if(!is.null(detect.early)){
+      # Rerun all peak extraction, with extrapolation
+      extrapolate_extract_peak <- function(y, h, args){
+        require(forecast)
+        # Reverse because peaks are missed in the beginning of series
+        # Forecast with arima model
+        yr <- rev(y)
+        fit <- auto.arima(yr)
+        y_extend <- rev(c(yr, forecast(fit, h = h)$mean))
+        # Extract peaks and trim back to initial size
+        args[["y"]] <- y_extend
+        args[["detect.early"]] <- NULL # Prevent infinite recursion
+        args[["show.warning"]] <- FALSE
+        args[["recenter"]] <- NULL
+        args[["filter.thresh"]] <- NULL
+        print(args)
+        peaks <- do.call(extract_peak, args)
+        return(peaks[(h+1):nrow(peaks)])
+      }
+      dt.out <- extrapolate_extract_peak(y, detect.early, argList)
+      print(dt.out)
+    }
+    
+    # Centering (repositioning to peak maximum)
+    if(!is.null(recenter)){
+      recenter_peaks <- function(y, positions, tol.winlen){
+        #y: signal with potentially several single peaks
+        #positions boolean indicating max_pos of peaks
+        #recenter peaks at maximum around mask in tolerance window
+        pos_recenter <- rep(FALSE, length(y))
+        for(pos in positions){
+          left <- ifelse((pos-tol.winlen) < 1, 1, pos-tol.winlen)
+          right <- ifelse((pos+tol.winlen) > length(y), length(y), pos+tol.winlen)
+          shift <- which.max(y[left:right]) - 1
+          pos_recenter[left + shift] <- TRUE
+        }
+        return(pos_recenter)
+      }
+      # Apply to each method
+      for(j in 2:ncol(dt.out)){
+        dt.out[, (colnames(dt.out)[j]) := recenter_peaks(signal, which(dt.out[[j]]), recenter)]
+      }
+    }
+    
+    # Filtering based on amplitude (in detrended data)
+    if(!is.null(filter.thresh)){
+      filter_in_window <- function(y, positions, thresh){
+        y_filtered <- rep(FALSE, length(y))
+        for(pos in positions){
+          if(y[pos] >= thresh) y_filtered[pos] <- TRUE
+        }
+        return(y_filtered)
+      }
+      # Add detrended signal to output and reorder
+      dt.out[, detrended.signal := signal - rollex(signal, filter.winlen)]
+      setcolorder(dt.out, c("signal", "detrended.signal", method))
+      for(j in 3:ncol(dt.out)){
+        dt.out[, (colnames(dt.out)[j]) := filter_in_window(detrended.signal, which(dt.out[[j]]), filter.thresh)]
+      }
     }
     return(dt.out)
   }
